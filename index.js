5af28a1c2948a1300a5c32ca29d8113e4e96322b
daniel-990 <danielarango990@gmail.com>
2023-01-17 10:30:22 -0500

    se actualizar el index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const axios = require('axios');
 
 //const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 const token = config.get('tokens.telegramBot');
+const usuario1 = 2032844097;
+const usuario2 = 177279577;
 //token.tokens.telegramBot
 //console.log(token);
 
@@ -52,13 +54,34 @@ bot.on('message', (msg) => {
   //monitoreo de datos del bot
   console.log(opciones.reply_markup);
   // se imprimen las respuestas
-  //bot.sendMessage(chatId, 'su mensaje fue recivido \nsu ID de chat es: '+chatId);
+  
+  //datos de retorno
   console.log("id de chat: "+chatId);
   console.log("nombre: "+msg.chat.first_name);
   console.log("mensaje id: "+msg.message_id);
+  console.log("mensaje: "+msg.text);
   //validacion de opciones
-  if(msg.text == "/fecha"){
 
+    //custom chats
+      if(chatId === usuario1 && msg.text == "Hola"){
+        bot.sendMessage(chatId,`Hola ${msg.chat.first_name} soy un Bot de pruebas, si ingresas /coin te dare el dato de las monedas digitales y precio actual, si ingresas /fecha te doy la fecha actual y \ncreo que ya nos conociamos de antes, esta nota es para ti, te la deja un tal @xorroPerro quieres saber cual es la nota? /si, /no`);
+      }
+      if(chatId === usuario1 && msg.text == "/si"){
+        bot.sendMessage(chatId,`Imaginar lo imposible, tratar de merecer un corazon inquieto, alternar el instante entre escritos de Bukowski, cervezas y un beso con el sabor a la mujer amada`);
+        console.log("entro en la condicion del si");
+        console.log("mensaje2: "+msg.text);
+      }
+      if(chatId === usuario1 && msg.text == "/no"){
+        bot.sendMessage(chatId,`Bueno igual no fue la nota pero para re-escuchar un poco en la mañana \nesta cancion que te deja mi creador: https://www.youtube.com/watch?v=p6E24jdWR-s&ab_channel=enlaterraza`);
+      }
+    //custom chats
+
+      if(chatId != usuario1){
+        bot.sendMessage(chatId,`Hola ${msg.chat.first_name} este es un Bot de pruebas, \ningresa /coin, para saber el tipo de monedas digitales o \n/fecha, para saber la fecha actual`);
+      }
+
+
+  if(msg.text == "/fecha"){
     let now= new Date();
     console.log('La fecha actual es',now);
     console.log('UNIX time:',now.getTime());
@@ -66,7 +89,7 @@ bot.on('message', (msg) => {
     
     //bot.sendMessage(chatId, "la opcion es: "+msg.text);
   }
-  if(msg.text=="/coin"){
+  if(msg.text == "/coin"){
     axios({
       method: 'get',
       url: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
@@ -102,6 +125,6 @@ bot.on('message', (msg) => {
     //       `);
     //     })
     //   });
-    bot.sendMessage(chatId, "no hay datos aun");
+    bot.sendMessage(chatId, "aun en construccion");
   }
 });
